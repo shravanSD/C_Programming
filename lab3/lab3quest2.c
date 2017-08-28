87f566a03230e978a42c63cd56bd76e4d3b99699
Shravan Dwarka <liizalbert_1304@live.com>
2017-08-28 23:47:44 +0400

    author: Shravan

--- lab3/lab3quest2.c
+++ lab3/lab3quest2.c
@@ -7,15 +7,26 @@ struct student
     int age;
 };
 
-void inputStudent(struct student *(s[]), int n);
-void displayStudent(struct student (s[])), int n;
+void inputStudent(struct student *s, int n);
+void displayStudent(struct student , int n);
 
 void main ()
 {
+    struct student x[20];
+    int i, j;
 
+    printf("Please enter number of students: ");
+    scanf("%d", &i);
+
+    for (j = 0; j < i; j++)
+    {
+        inputStudent(&(x[j]), i);
+    }
+
+    printf("The youngest student is %s %s living at %s having %d years old.", x[youngestStud(x, i)].oname, x[youngestStud(x, i)].sname, x[youngestStud(x, i)].address, x[youngestStud(x, i)].age);
 }
 
-void inputStudent(struct student *(s[]), int n)
+void inputStudent(struct student *s, int n)
 {
     printf("Input surname: ");
     scanf("%s", s->sname);
@@ -30,10 +41,24 @@ void inputStudent(struct student *(s[]), int n)
     scanf("%d", &s->age);
 }
 
-void displayStudent(struct student (s[]), int n)
+void displayStudent(struct student s, int n)
 {
     printf("Surname:\t%s\n", s.sname);
     printf("Othername:\t%s\n", s.oname);
     printf("Address:\t%s\n", s.address);
     printf("Age:\t\t%d\n", s.age);
+}
+
+int youngestStud(struct student (s[]), int n)
+{
+    int i;
+    int min = s[0].age;
+    for (i = 0; i < n; i++)
+    {
+        if (s[i].age < min)
+        {
+            min = i;
+        }
+    }
+    return min;
 }
